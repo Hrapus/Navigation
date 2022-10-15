ff4040d0c78803c037e279c5c8c628c605e22ffd
Hrapus <hrapus@gmail.com>
2022-10-15 13:05:48 -0400

    navigation.fragments

---
+++ app/src/main/java/com/example/navigation/fragments/MainFragment.kt
@@ -0,0 +1,43 @@
+package com.example.navigation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.navigation.R
+
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val batFrag1 = view.findViewById<Button>(R.id.buttonFrag_1)
+        val batFrag2 = view.findViewById<Button>(R.id.buttonFrag_2)
+
+        val controller = findNavController()
+
+        batFrag1.setOnClickListener {
+            controller.navigate(R.id.fragment1)
+        }
+
+        batFrag2.setOnClickListener {
+            controller.navigate(R.id.fragment2)
+        }
+    }
+
+
+}
